5c35466a5a4fc478b6121989a07ab8059060c885
PetrunevaTasia <hext2004t@gmail.com>
2025-04-16 23:40:00 +0700

    task is completed

--- src/Task3.hs
+++ src/Task3.hs
@@ -1,10 +1,23 @@
 {-# OPTIONS_GHC -Wall #-}
+{-# LANGUAGE InstanceSigs #-}
 -- The above pragma enables all warnings
 
 module Task3 where
 
-import Task2 (Stream)
+import Task2 (Stream (..), fromList, showHelper)
 import Data.Ratio (Ratio)
+import Data.Foldable(toList)
+
+instance Foldable Series where
+  foldMap :: Monoid m => (a -> m) -> Series a -> m
+  foldMap f = foldr (\a m -> f a <> m) mempty
+
+  toList :: Series a -> [a]
+  toList (Series a) = toList a 
+
+instance Show a => Show (Series a) where
+  show :: Show a => Series a -> String
+  show (Series stream) = showHelper 0 stream
 
 -- | Power series represented as infinite stream of coefficients
 -- 
@@ -40,7 +53,37 @@ newtype Series a = Series
 -- [0,1,0,0,0,0,0,0,0,0]
 --
 x :: Num a => Series a
-x = error "TODO: define x"
+x = Series (fromList 0 [0, 1])
+
+instance (Num a) => Num (Series a) where
+
+    fromInteger :: Num a => Integer -> Series a
+    fromInteger n = Series (fromList 0 [fromInteger n])
+
+    negate :: Num a => Series a -> Series a
+    negate (Series stream) = Series (fmap (* (-1)) stream)
+
+    (+) :: Num a => Series a -> Series a -> Series a
+    (+) (Series a) (Series b) = Series (plusInStreams a b)
+
+    (*) :: Num a => Series a -> Series a -> Series a
+    (*) (Series stream1) (Series stream2) = Series (mulInStreams stream1 stream2)
+
+    abs :: Num a => Series a -> Series a
+    abs (Series a) = Series (fmap abs a)
+
+    signum :: Num a => Series a -> Series a
+    signum (Series a) = Series (fmap signum a)
+
+plusInStreams :: Num a => Stream a -> Stream a -> Stream a
+plusInStreams (Stream f1 s1) (Stream f2 s2) = Stream (f1 + f2) (plusInStreams s1 s2)
+
+minusInStreams :: Num a => Stream a -> Stream a -> Stream a
+minusInStreams (Stream f1 s1) (Stream f2 s2) = Stream (f1 - f2) (minusInStreams s1 s2)
+
+mulInStreams :: Num a => Stream a -> Stream a -> Stream a
+mulInStreams (Stream f1 s1) (Stream f2 s2) = 
+  Stream (f1 * f2) (fmap (f1 *) s2 `plusInStreams` (s1 `mulInStreams` Stream f2 s2))
 
 -- | Multiplies power series by given number
 -- 
@@ -58,7 +101,22 @@ x = error "TODO: define x"
 --
 infixl 7 *:
 (*:) :: Num a => a -> Series a -> Series a
-(*:) = error "TODO: define (*:)"
+(*:) val (Series stream) = Series(fmap (val *) stream)
+
+
+
+instance (Fractional a) => Fractional (Series a) where
+
+  fromRational :: Fractional a => Rational -> Series a
+  fromRational n = Series (fromList 0 [fromRational n])
+
+  (/) :: Fractional a => Series a -> Series a -> Series a
+  (/) (Series stream1) (Series stream2) = Series (divInStreams stream1 stream2)
+
+divInStreams :: Fractional a => Stream a -> Stream a -> Stream a
+divInStreams (Stream f1 s1) (Stream f2 s2) = 
+  Stream q ((s1 `minusInStreams` fmap (q *) s2) `divInStreams` Stream f2 s2)
+  where q = f1 / f2
 
 -- | Helper function for producing integer
 -- coefficients from generating function
@@ -70,7 +128,7 @@ infixl 7 *:
 -- [2,3,0,0,0,0,0,0,0,0]
 --
 gen :: Series (Ratio Integer) -> Stream Integer
-gen = error "TODO: define gen"
+gen (Series stream) = coefficients (Series (fmap round stream))
 
 -- | Returns infinite stream of ones
 --
@@ -80,7 +138,7 @@ gen = error "TODO: define gen"
 -- [1,1,1,1,1,1,1,1,1,1]
 --
 ones :: Stream Integer
-ones = error "TODO: define ones"
+ones = gen (1 / (1 - x))
 
 -- | Returns infinite stream of natural numbers (excluding zero)
 --
@@ -90,7 +148,7 @@ ones = error "TODO: define ones"
 -- [1,2,3,4,5,6,7,8,9,10]
 --
 nats :: Stream Integer
-nats = error "TODO: define nats (Task3)"
+nats = gen (1 / (1 - 2 * x + x * x))
 
 -- | Returns infinite stream of fibonacci numbers (starting with zero)
 --
@@ -100,5 +158,4 @@ nats = error "TODO: define nats (Task3)"
 -- [0,1,1,2,3,5,8,13,21,34]
 --
 fibs :: Stream Integer
-fibs = error "TODO: define fibs (Task3)"
-
+fibs = gen (x / (1 - x - x * x))
